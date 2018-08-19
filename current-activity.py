8830a2428960096f00d27e16d96f23becde59087
don undeen <don.undeen@metmuseum.org>
2018-08-19 18:26:44 -0400

    current-activity.py

--- current-activity.py
+++ current-activity.py
@@ -6,8 +6,27 @@
 # are staffing services.
 
 from datetime import datetime
+# Import standard python modules
+import time
+
+# Import Adafruit IO REST client.
+from Adafruit_IO import Client, Feed
 
 import lh3.api
+
+# Set to your Adafruit IO key.
+# Remember, your key is a secret,
+# so make sure not to publish it when you publish this code!
+ADAFRUIT_IO_KEY = ''
+
+# Set to your Adafruit IO username.
+# (go to https://accounts.adafruit.com to find your username)
+ADAFRUIT_IO_USERNAME = 'gumakerhub'
+ADAFRUIT_IO_FEEDNAME = 'makerhubevents.signagemessage'
+
+# Create an instance of the REST client.
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
 
 client = lh3.api.Client()
 
@@ -21,6 +40,8 @@ for chat in chats:
     print(chat)
     if chat['ended']:
         continue
+    if chat['queue'] != 'gt-makerhub':
+        continue
 
     # Only count those starting in the last 15 minutes.
     started = datetime.strptime(chat['started'], '%Y-%m-%d %H:%M:%S')
@@ -31,29 +52,15 @@ for chat in chats:
         num_active += 1
     else:
         num_unanswered += 1
+        print("this unanswered chat")
+        print(chat['queue'])
 
 print('{} active chats, {} unanswered'.format(num_active, num_unanswered))
 
 if num_unanswered > 0:
     print ("SEND AN ALERT NOW!!!")
+    aio.send_data(ADAFRUIT_IO_FEEDNAME, "Incoming Chat!")
 
-# For each user...
-client.set_options(version = 'v1')
-users = client.all('users')
-num_users = 0
-for user in users.get_list():
-    if user['show'] != u'chat' and user['show'] != u'available':
-        continue
 
-    # Is that user staffing any queue?
-    staffing = False
-    assignments = users.one(user['id']).all('assignments').get_list()
-    for assignment in assignments:
-        if assignment['enabled']:
-            staffing = True
 
-    # If so, then great!
-    if staffing:
-        num_users += 1
 
-print('{} online operators'.format(num_users))
